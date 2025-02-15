23d57b3380f41cac5a74e2a6ce98a9db722b8daa
Your Name <mhdameent2006@gmail.com>
2025-02-15 16:37:53 +0530

    NEW COMMIT

--- Day-2/sample.ts
+++ Day-2/sample.ts
@@ -1,21 +1,21 @@
-class Person {
-    name?: string | number;
+// class Person {
+//     name?: string | number;
 
-    constructor(name?: string | number) {
-        this.name = name;
-    }
+//     constructor(name?: string | number) {
+//         this.name = name;
+//     }
 
-    getName(): string | number | undefined {
-        return this.name;
-    }
-}
+//     getName(): string | number | undefined {
+//         return this.name;
+//     }
+// }
 
-let person1 = new Person('Ameen');
-let person2 = new Person();
-let person3 = new Person(42);
+// let person1 = new Person('Ameen');
+// let person2 = new Person();
+// let person3 = new Person(42);
 
-console.log(person1.getName()); // Output: Ameen
-console.log(person2.getName()); // Output: undefined
-console.log(person3.getName()); // Output: 4
+// console.log(person1.getName()); // Output: Ameen
+// console.log(person2.getName()); // Output: undefined
+// console.log(person3.getName()); // Output: 4
 
 
